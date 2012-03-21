98f90a1cb8eef3617ed926487c47d1f46aea83a5
Anorov <phitapig@gmail.com>
2012-03-21 05:40:38 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-VERSION = "1.02"
+VERSION = "1.04"
 
 setup(
-name = "SocksiPy-branch",
+name = "PySocks",
 version = VERSION,
 description = "A Python SOCKS module",
 long_description = "This Python module allows you to create TCP connections through a SOCKS proxy without any special effort.",
 url = "http://socksipy.sourceforge.net/",
 author = "Dan-Haim",
-author_email="negativeiq@users.sourceforge.net",
+author_email = "negativeiq@users.sourceforge.net",
 license = "BSD",
-py_modules=["socks"]
+py_modules=["socks", "sockshandler"]
 )
-
